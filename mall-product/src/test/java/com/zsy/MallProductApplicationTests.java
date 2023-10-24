ad26ff773b4ef50c44326396c054454aae5679f4
liqiuliang <liqiuliang@bilibili.com>
2023-10-25 01:40:51 +0800

    test

--- mall-product/src/test/java/com/zsy/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/zsy/MallProductApplicationTests.java
@@ -2,8 +2,6 @@ package com.zsy;
 
 import com.zsy.product.entity.BrandEntity;
 import com.zsy.product.service.BrandService;
-import com.zsy.product.test.factorStragerModel.AbstractHandler;
-import com.zsy.product.test.factorStragerModel.Factory;
 import org.junit.jupiter.api.Test;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,15 +43,4 @@ class MallProductApplicationTests {
     public void test2() {
         System.out.println(redissonClient);
     }
-
-    @Test
-    public void test3() {
-        String name = "b";
-        AbstractHandler abstractHandler = Factory.get(name);
-        abstractHandler.BBB(name);
-        name = "a";
-        AbstractHandler abstractHandler1 = Factory.get(name);
-        abstractHandler1.AAA(name);
-    }
-
 }
